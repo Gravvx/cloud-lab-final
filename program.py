27831c9acd19f54a27fd8a297d9b49d6fb7ae6cb
Gravvx <forstudywork24x7@gmail.com>
2026-05-07 10:41:16 +0530

    Add output display to program.py

---
+++ program.py
@@ -0,0 +1,4 @@
+print("==============================================================================")
+print(" this is the output of the GITHUB FILE ")
+print("==============================================================================")
+print(" Name : Pandey Gaurav \n subject : Cloud Computing [ LP - 2] ")
